380f88459e3fc55eb332fc00d3685b5b5b36f25d
Harpreet Gill <harpreet_gill_@outlook.com>
2020-02-14 13:17:19 -0500

    Added Add Order

--- PassionProject - Copy/PassionProject/Controllers/OrderController.cs
+++ PassionProject - Copy/PassionProject/Controllers/OrderController.cs
@@ -27,7 +27,7 @@ namespace PassionProject.Controllers
             
             //also need the jerseys for that order
             //select * from jerseys and inner join with orderjerseys on jersey id where order id = passed value
-            string query = "select * from Jerseys inner join OrderJerseys on Jerseys.jerseyId = OrderJerseys.Jersey_jerseyId where Order_orderId = @id";
+            string query = "select * from Jerseys inner join OrderJerseys on Jerseys.jerseyId = OrderJerseys.Jersey_jerseyId where OrderJerseys.Order_orderId = @id";
             SqlParameter parameter = new SqlParameter("@id", id);
             List<Jersey> jerseyOrders = db.Jerseys.SqlQuery(query, parameter).ToList();
 
@@ -47,15 +47,25 @@ namespace PassionProject.Controllers
         public ActionResult AddJersey(int id, int jerseyId)
         {
                 Debug.WriteLine("Order is " + id + " and jerseyId is " + jerseyId+" and price is ");
-
+            //check if the jersey is already in the order
+            string check = "select * from jerseys inner join OrderJerseys on OrderJerseys.Jersey_jerseyid = jerseys.jerseyId where Jersey_jerseyid = @id and OrderJerseys.Order_orderid = @orderId";
+            SqlParameter[] sql = new SqlParameter[2];
+            sql[0] = new SqlParameter("@id", jerseyId);
+            sql[1] = new SqlParameter("@orderId", id);
+            List<Jersey> list = db.Jerseys.SqlQuery(check, sql).ToList();
+            if(list.Count <= 0)
+            {
                 //insert the order id and jersey id into the orderJerseys table
                 string query = "insert into OrderJerseys (Order_orderId, Jersey_jerseyId) values (@orderId,@jerseyId)";
                 SqlParameter[] sqlParameter = new SqlParameter[2];
                 sqlParameter[0] = new SqlParameter("@orderId", id);
                 sqlParameter[1] = new SqlParameter("@jerseyid", jerseyId);
-                
+
                 //execute command 
                 db.Database.ExecuteSqlCommand(query, sqlParameter);
+            }
+                
+                
 
                 //update price of order
                 //string priceQuery = "update Orders set orderPrice=@price where orderid=@orderId";
@@ -90,6 +100,40 @@ namespace PassionProject.Controllers
             ////ececute update price command
             //db.Database.ExecuteSqlCommand(priceQuery, sql);
             return RedirectToAction("Show/" + id);
+        }
+        public ActionResult Add()
+        {
+            //get list of all jerseys
+            string alljerseys = "select * from Jerseys";
+            List<Jersey> jerseys = db.Jerseys.SqlQuery(alljerseys).ToList();
+
+            //get list of all jerseys
+            string allCustomer = "select * from Customers";
+            List<Customer> customer = db.Customers.SqlQuery(allCustomer).ToList();
+
+            //get order id
+            //string getid = "select max(orderId) from Orders";
+            //Order order = db.Orders.SqlQuery(getid).First();
+
+            AddOrder addOrder = new AddOrder();
+            addOrder.Jerseys = jerseys;
+            addOrder.Customers = customer;
+            return View(addOrder);
+        }
+        [HttpPost]
+        public ActionResult Add(int customerId)
+        {
+            string query = "insert into Orders (customerid,orderDate) values(@customerId,@date)";
+            SqlParameter[] sqlParameter = new SqlParameter[2];
+            sqlParameter[0] = new SqlParameter("@customerId", customerId);
+            sqlParameter[1] = new SqlParameter("@date", DateTime.Now);
+
+            db.Database.ExecuteSqlCommand(query, sqlParameter);
+            
+            
+            
+            
+            return RedirectToAction("List");
         }
     }
 }
